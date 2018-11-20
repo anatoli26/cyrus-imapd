0b8f118085c69656e5bbd7462ca9c26cdbde947c
Ken Murchison <murch@fastmail.com>
2018-11-20 10:05:56 -0500

    don't remove messages annotations when RENAMing a mailbox in place

--- imap/annotate.c
+++ imap/annotate.c
@@ -3782,7 +3782,8 @@ static int rename_cb(const char *mboxname __attribute__((unused)),
 }
 
 EXPORTED int annotate_rename_mailbox(struct mailbox *oldmailbox,
-                                     struct mailbox *newmailbox)
+                                     struct mailbox *newmailbox,
+                                     int copy)
 {
     /* rename one mailbox */
     char *olduserid = mboxname_to_userid(oldmailbox->name);
